84af7cbf3381e85c9730985e260c442993ec6cdc
Eric Chrobak <eric@ericchrobak.com>
2018-09-01 04:47:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       matchItem = @@items.detect { |a|
         a.name == item
       }
-
+      binding.pry
       if matchItem
         resp.write "#{matchItem.price}"
         resp.status = 200
