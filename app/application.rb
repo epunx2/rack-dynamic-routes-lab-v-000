5c6867634ef022bd2a57879e03ee3547867d253b
Eric Chrobak <eric@ericchrobak.com>
2018-09-01 05:03:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,11 @@
 class Application
+
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    @@items = []
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
